33d935e2366eda52b09f55fe71f5fb286023f928
joshrandall8478 <joshrandall8478@gmail.com>
2023-08-19 10:08:46 -0400

    fixed kitty script

--- install-kitty-conf.sh
+++ install-kitty-conf.sh
@@ -3,7 +3,7 @@
 cwd=$(pwd)
 
 if [ -d ~/.config/kitty ]; then
-        read -p "kitty config found, clone your current one as kitty.old?" kittyChoice
+        read -p "kitty config found, clone your current one as kitty.old? [y/n] " kittyChoice
         if [ ${kittyChoice,,} == "y" ]; then
                 mv ~/.config/kitty ~/.config/kitty.old
                 cp -r $cwd/.config/kitty ~/.config
