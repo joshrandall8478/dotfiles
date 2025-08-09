3a7e106d4d7373f3da1e89e3d7da372530ba55f0
Joshua Randall <josh@joshrandall.net>
2025-08-09 00:43:00 -0400

    docs finished for now

--- scripts/executable_install-vscode-extensions.sh
+++ scripts/executable_install-vscode-extensions.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-while read line; do code --install-extension "$line"; done < vscode-extensions.txt
+while read line; do code --install-extension "$line"; done < ~/scripts/vscode-extensions.txt
